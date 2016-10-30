4f6995cc87c59cf463f40cbcf811168ad2d6fee7
Minori Hiraoka <Mnkai@users.noreply.github.com>
2016-10-30 23:32:09 +0900

    Fix helper install location

--- installHelper.sh
+++ installHelper.sh
@@ -3,5 +3,5 @@
 #register kernel module, to start without rebooting
 modprobe fingermarathon
 
-echo "cat /sys/module/fingermarathon/parameters/keycount" > /bin/fingermarathon
-chmod +x /bin/fingermarathon
+echo "cat /sys/module/fingermarathon/parameters/keycount" > /usr/bin/fingermarathon
+chmod +x /usr/bin/fingermarathon
