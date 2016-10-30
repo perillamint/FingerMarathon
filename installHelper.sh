6d4537ff87f19f1729a6cd4c9dc58d79feadf9f5
Minori Hiraoka <Mnkai@users.noreply.github.com>
2016-10-30 23:33:45 +0900

    Automatically insert fingermarathon module

--- installHelper.sh
+++ installHelper.sh
@@ -3,5 +3,8 @@
 #register kernel module, to start without rebooting
 modprobe fingermarathon
 
+#Automatically insert module after rebooting
+echo "fingermarathon" > /etc/modules-load.d/fingermarathon.conf
+
 echo "cat /sys/module/fingermarathon/parameters/keycount" > /usr/bin/fingermarathon
 chmod +x /usr/bin/fingermarathon
